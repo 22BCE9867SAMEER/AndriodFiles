839c4600c88fecbc45189f82ba52a037cffbc4fb
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-22 22:12:38 +0530

    java to Kotlin

---
+++ app/src/main/java/com/sameer/myprojects/kotlinex/Student.kt
@@ -0,0 +1,3 @@
+package com.sameer.myprojects.kotlinex
+
+class Student(var name: String, var age: Int, var address: String)
---
+++ app/src/main/java/com/sameer/myprojects/kotlinex/user.kt
@@ -0,0 +1,7 @@
+package com.sameer.myprojects.kotlinex
+fun main(){
+
+
+    val myobj:Student = Student("sammer",123,"chhenai")
+    println(myobj.name)
+}
