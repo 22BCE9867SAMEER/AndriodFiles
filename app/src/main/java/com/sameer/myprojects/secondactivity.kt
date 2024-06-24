eb695baa2f5f0608b2c47fe4aaf5a320735280cd
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-24 13:32:34 +0530

    INTENTS

--- app/src/main/java/com/sameer/myprojects/secondactivity.kt
+++ app/src/main/java/com/sameer/myprojects/secondactivity.kt
@@ -1,23 +1,16 @@
 package com.sameer.myprojects
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class secondactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val bnt = findViewById<Button>(R.id.button)
-        bnt.setOnClickListener {
-            intent = Intent(this,secondactivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_secondactivity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
