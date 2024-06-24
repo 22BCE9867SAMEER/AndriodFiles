f5c0d46a73118ef756a135f6e29932659f0ef1ba
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-24 14:36:02 +0530

    debug , call stack

--- app/src/main/java/com/sameer/myprojects/MainActivity.kt
+++ app/src/main/java/com/sameer/myprojects/MainActivity.kt
@@ -16,11 +16,14 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val bnt = findViewById<Button>(R.id.button)
+
         bnt.setOnClickListener {
             intent = Intent(this,secondactivity::class.java)
-           throw NullPointerException("demo vit")
+           //throw NullPointerException("demo vit")
+            //val c = 10+20
             startActivity(intent)
         }
+        add(10,20)
         Log.i(tag ,"activity is getting creeatead")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -51,4 +54,15 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.v(tag,"activity is destroyed")
     }
+    fun add(a:Int,B:Int){
+        val c = a+B
+        mul(a,B)
+    }
+    fun mul(a:Int ,B:Int){
+        div(a,B)
+    }
+    fun div(a:Int,B:Int){
+        val c = a/B;
+    }
+
 }
