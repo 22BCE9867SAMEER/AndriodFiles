4f7cc9b282faa95ef169d1ca095bb2c0303d962f
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-24 19:07:21 +0530

    Extention function

--- app/src/main/java/com/sameer/myprojects/kotlinex/user.kt
+++ app/src/main/java/com/sameer/myprojects/kotlinex/user.kt
@@ -3,6 +3,21 @@ fun main() {
 
 
     val myobj:Student = Student("sammer",123,"chhenai")
-    println(myobj.name)}
-    //val obj:print=print()
+    println(myobj.name)
+
+val name:String = "Sameer"
+    println(name.commaSeparated("some other string"))
+
+
+
+
+}
+
+private fun Any.commaSeparated(s: String): Any {
+    return "commasepareted "+s;
+
+}
+
+
+//val obj:print=print()
    // obj.somegthing()
