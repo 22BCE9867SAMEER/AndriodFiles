432efe2ea149b028635d23ab4ab3a1c89402cfea
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-24 20:25:24 +0530

    spinners

--- app/src/main/java/com/sameer/myprojects/secondactivity.kt
+++ app/src/main/java/com/sameer/myprojects/secondactivity.kt
@@ -1,20 +1,40 @@
 package com.sameer.myprojects
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class secondactivity : AppCompatActivity() {
+class secondactivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    lateinit var myspinner:Spinner
+    var TAG = secondactivity::class.java.simpleName //"secondactivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_secondactivity)
+        myspinner = findViewById(R.id.spinner)
+        myspinner.onItemSelectedListener=this;
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+    @Override
+    override fun onItemSelected(adapter:AdapterView<*>?, view: View?, postion:Int, id:Long){
+        val item = adapter?.getItemAtPosition(postion).toString();
+        Log.i(TAG ,item)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
