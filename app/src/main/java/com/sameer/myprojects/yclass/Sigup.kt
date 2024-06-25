0ea7b5e7ad953bec2f60ce92bd11e0470faf19ee
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-25 20:46:53 +0530

    DataBase-sigin

--- app/src/main/java/com/sameer/myprojects/yclass/Sigup.kt
+++ app/src/main/java/com/sameer/myprojects/yclass/Sigup.kt
@@ -1,7 +1,9 @@
 package com.sameer.myprojects.yclass
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,8 +14,8 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.sameer.myprojects.R
 
-class sigup : AppCompatActivity() {
-     lateinit var database: DatabaseReference
+class Sigup : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,21 +25,28 @@ class sigup : AppCompatActivity() {
         val password1=findViewById<TextInputEditText>(R.id.password)
         val uid = findViewById<TextInputEditText>(R.id.id)
         val bnt = findViewById<Button>(R.id.button)
+        val textview = findViewById<TextView>(R.id.textView2)
 
         bnt.setOnClickListener {
-            val user_name = uname.text.toString()
+            val username = uname.text.toString()
             val mail = email.text.toString()
-            val password =password1.text.toString()
+            val password = password1.text.toString()
             val uniqueid = uid.text.toString()
-            val user = User(user_name,mail,password,uniqueid)
-            database=FirebaseDatabase.getInstance().getReference("Data")
+            val user = User(username, mail, password, uniqueid)
+            database = FirebaseDatabase.getInstance().getReference("Data")
             database.child(uniqueid).setValue(user).addOnSuccessListener {
-                Toast.makeText(this,"user registered", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "user registered", Toast.LENGTH_SHORT).show()
             }.addOnSuccessListener {
-                Toast.makeText(this,"Failed ",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Failed ", Toast.LENGTH_SHORT).show()
             }
+        }
+        textview.setOnClickListener{
+            val intent = Intent(this,signin::class.java)
+            startActivity(intent)
 
         }
+
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/sameer/myprojects/yclass/signin.kt
+++ app/src/main/java/com/sameer/myprojects/yclass/signin.kt
@@ -1,21 +1,74 @@
 package com.sameer.myprojects.yclass
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.sameer.myprojects.R
 
 class signin : AppCompatActivity() {
+    lateinit var databasereference:DatabaseReference
+    companion object{
+        const val KEY1 = "com.sameer.myprojects.yclass.KEY"
+        const val KEY2 = "com.sameer.myprojects.yclass.KEY"
+        const val KEY3 = "com.sameer.myprojects.yclass.KEY"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_signin)
+        val bnt = findViewById<Button>(R.id.button2)
+        val id = findViewById<TextInputEditText>(R.id.uid)
+        bnt.setOnClickListener {
+            //check the id
+            //open other activity
+            //welcome
+            val uniqueid = id.text.toString()
+            if(uniqueid.isNotEmpty()){
+                readdata(uniqueid)
+            }
+            else{
+                Toast.makeText(this,"enter userid ",Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun readdata(uniqueid: String) {
+        databasereference = FirebaseDatabase.getInstance().getReference("Data")
+        databasereference.child(uniqueid).get().addOnSuccessListener {
+            if(it.exists()){
+                val email = it.child("mail").value
+                val uname = it.child("name").value
+                val uid =it.child("uniqueid").value
+                //val intentwelcome = Intent(this,)
+                val intent = Intent(this,Welcome::class.java)
+                intent.putExtra(KEY1,email.toString())
+                intent.putExtra(KEY2,uname.toString())
+                intent.putExtra(KEY3,uid.toString())
+                startActivity(intent)
+
+
+            }else{
+                Toast.makeText(this,"user not found ",Toast.LENGTH_SHORT).show()
+
+            }
+        }.addOnFailureListener{
+            Toast.makeText(this,"error in database",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
