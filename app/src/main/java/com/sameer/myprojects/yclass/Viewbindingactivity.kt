83b7cb910c9bf8fe08a6c2e81eba60088ead39c4
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-26 20:33:15 +0530

    checkbox implemntation

--- app/src/main/java/com/sameer/myprojects/yclass/Viewbindingactivity.kt
+++ app/src/main/java/com/sameer/myprojects/yclass/Viewbindingactivity.kt
@@ -1,25 +1,37 @@
 package com.sameer.myprojects.yclass
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.collection.LLRBNode.Color
+import com.google.firebase.database.collection.LLRBNode.Color.*
 import com.sameer.myprojects.R
 import com.sameer.myprojects.databinding.ActivityMainBinding
+import com.sameer.myprojects.databinding.ActivityViewbindingactivityBinding
 
 class Viewbindingactivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding:ActivityViewbindingactivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityViewbindingactivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //here no neede of findview by id
-binding.button.setOnClickListener {
-    Toast.makeText(this, " sameer", Toast.LENGTH_SHORT).show()
-}
+        binding.button2.setOnClickListener {
+            if(binding.checkbox.isChecked){
+                //open a new screen
+
+
+            }else{
+                binding.checkbox.buttonTintList = ColorStateList.valueOf(android.graphics.Color.RED)
+                Toast.makeText(this,"please aceept on terms and conditions",Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
