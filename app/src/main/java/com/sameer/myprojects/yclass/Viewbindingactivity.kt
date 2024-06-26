311ff01dd769c6042f5d31a6ff827dc20dd88c11
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-06-26 14:57:43 +0530

    ViewBinding

--- app/src/main/java/com/sameer/myprojects/yclass/Viewbindingactivity.kt
+++ app/src/main/java/com/sameer/myprojects/yclass/Viewbindingactivity.kt
@@ -1,17 +1,26 @@
 package com.sameer.myprojects.yclass
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sameer.myprojects.R
+import com.sameer.myprojects.databinding.ActivityMainBinding
 
 class Viewbindingactivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_viewbindingactivity)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //here no neede of findview by id
+binding.button.setOnClickListener {
+    Toast.makeText(this, " sameer", Toast.LENGTH_SHORT).show()
+}
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
