10c23976c76820ece84fee58260bedd05c33518e
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-07-02 11:51:37 +0530

    add api to textview and displaying

--- app/src/main/java/com/sameer/myprojects/yclass/Api/ApiActivity.kt
+++ app/src/main/java/com/sameer/myprojects/yclass/Api/ApiActivity.kt
@@ -1,17 +1,59 @@
 package com.sameer.myprojects.yclass.Api
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sameer.myprojects.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class ApiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_api)
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(Apiinterface::class.java)
+
+        val retrofitData = retrofitBuilder.getProducts()
+        retrofitData.enqueue(object : Callback<Mydata?> {
+            override fun onResponse(call: Call<Mydata?>, response: Response<Mydata?>) {
+                //if api is scuuess , then use the dta of api and show
+                var responesebody = response.body()
+                val productlist = responesebody?.products
+                val collectDatainsb = StringBuilder()
+
+                if (productlist != null) {
+                    for(mydata in productlist){
+                        collectDatainsb.append(mydata.title+" ")
+
+
+
+                    }
+                    val tv = findViewById<TextView>(R.id.te)
+                    tv.text = collectDatainsb
+                }
+
+            }
+
+            override fun onFailure(call: Call<Mydata?>, t: Throwable) {
+                Log.d("ApiActivity","onfaliure"+t.message)
+
+            }
+        })
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
