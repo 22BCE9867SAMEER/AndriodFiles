45a274bce92d230aec786be2d02f1af9acbb5fe1
22BCE9867SAMEER <sameer.22bce9867@vitapstudent.ac.in>
2024-07-02 13:47:52 +0530

    api,json ,recyclerview and adapter complete implementation

--- app/src/main/java/com/sameer/myprojects/yclass/Api/ApiActivity.kt
+++ app/src/main/java/com/sameer/myprojects/yclass/Api/ApiActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sameer.myprojects.R
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,10 +18,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
 class ApiActivity : AppCompatActivity() {
+    lateinit var recyclerview :RecyclerView
+   lateinit var adaptermy :Myadapter2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_api)
+        recyclerview = findViewById(R.id.rv)
+
         val retrofitBuilder = Retrofit.Builder()
             .baseUrl("https://dummyjson.com/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -31,10 +37,16 @@ class ApiActivity : AppCompatActivity() {
             override fun onResponse(call: Call<Mydata?>, response: Response<Mydata?>) {
                 //if api is scuuess , then use the dta of api and show
                 var responesebody = response.body()
-                val productlist = responesebody?.products
-                val collectDatainsb = StringBuilder()
+                val productlist= responesebody?.products!!
 
-                if (productlist != null) {
+ //intialize the adapter
+                adaptermy = Myadapter2(this@ApiActivity,productlist)
+                recyclerview.adapter  = adaptermy
+                recyclerview.layoutManager = LinearLayoutManager(this@ApiActivity)
+
+             /*   val collectDatainsb = StringBuilder()
+
+               if (productlist != null) {
                     for(mydata in productlist){
                         collectDatainsb.append(mydata.title+" ")
 
@@ -43,7 +55,7 @@ class ApiActivity : AppCompatActivity() {
                     }
                     val tv = findViewById<TextView>(R.id.te)
                     tv.text = collectDatainsb
-                }
+                }*/
 
             }
 
